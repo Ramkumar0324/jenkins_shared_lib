c61c0806c3bbc81c5ecfe9bcfb87a6f8d0381a07
Ramkumar0324 <107704493+Ramkumar0324@users.noreply.github.com>
2023-11-18 21:15:48 +0530

    Delete vars/jfrog.groovy

--- vars/jfrog.groovy
+++
@@ -1,8 +0,0 @@
-def server = Artifactory.server '<artifactory id>'
-    def uploadSpec = '''{
-        "files": [{
-        "pattern": "<name of war or jar file>",
-        "target": "<artifactory repo>/path-to/war-or-jar/file/in-Artifactory"
-        }]
-    }'''
-    server.upload(uploadSpec)
