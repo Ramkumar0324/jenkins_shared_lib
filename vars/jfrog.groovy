09e28d42c0c58eed3b1cf804dcc4610dfdbd114a
Ramkumar0324 <107704493+Ramkumar0324@users.noreply.github.com>
2023-11-18 20:59:08 +0530

    Create jfrog.groovy

---
+++ vars/jfrog.groovy
@@ -0,0 +1,10 @@
+script {
+    def server = Artifactory.server '<artifactory id>'
+    def uploadSpec = '''{
+        "files": [{
+        "pattern": "<name of war or jar file>",
+        "target": "<artifactory repo>/path-to/war-or-jar/file/in-Artifactory"
+        }]
+    }'''
+    server.upload(uploadSpec) 
+}
